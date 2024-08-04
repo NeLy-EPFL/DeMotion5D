1b836950c43289c7076c12b650b95ac0a7a27e8c
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-08-04 15:51:21 +0200

    Rename `template` to `target`. Handle metadata better.

--- 1_make_vols.py
+++ 1_make_vols.py
@@ -9,34 +9,35 @@ import npimage
 #template_timepoint = 374
 template_timepoint = None
 planes = slice(1, -1)
-metadata = {
-    'space dimension': 3,
-    'space directions': [[0, 0, 1.5], [0, 1.128, 0], [1.133, 0, 0]],
-    'space units': ['microns', 'microns', 'microns']
-}
 
 fn = Path(sys.argv[1])
-im = npimage.load(fn)
+im, metadata = npimage.load(fn, return_metadata=True)
+if not metadata['space dimension'] == 4:
+    raise ValueError('Input image must have 4 dimensions')
+del metadata['space dimension']
+# Strip the time dimension from the metadata
+metadata['space directions'] = metadata['space directions'][1:, 1:]
+metadata['space units'] = metadata['space units'][1:]
 print('im.shape:', im.shape)
 
-target = fn.parent / f'{fn.stem}_demotion'
-target_timepoints = target / 'timepoints'
-target_timepoints.mkdir(exist_ok=True, parents=True)
+output_root = fn.parent / f'{fn.stem}_demotion'
+output_timepoints = output_root / 'timepoints'
+output_timepoints.mkdir(exist_ok=True, parents=True)
 
 
 def get_vol(t, planes=planes):
     return im[t, planes, ...]
 
 
-if template_timepoint is not None and not (target / 'template.nrrd').exists():
-    template = get_vol(template_timepoint)
-    npimage.save(template,
-                 target / 'template.nrrd',
+if target_timepoint is not None and not (output_root / 'target.nrrd').exists():
+    target = get_vol(target_timepoint)
+    npimage.save(target,
+                 output_root / 'target.nrrd',
                  metadata=metadata)
 
 interval = 1
 for t in tqdm(range(0, im.shape[0], interval)):
     npimage.save(get_vol(t),
-                 target_timepoints / f't{t:04d}.nrrd',
+                 output_timepoints / f't{t:04d}.nrrd',
                  metadata=metadata,
                  overwrite=True)
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Do be mindful of the fact that it's important to have the voxel size specified i
 
 ## Steps
 1. Make a sequence of nrrd files, one per time point per channel
-1. Run elastix to register each timepoint to a common template
+1. Run elastix to register each timepoint to a common target
 1. Check for poor results and troubleshoot/rerun those alignments
 1. Apply the transforms to each timepoint in other color channels
 1. Combine all transformed timepoints into a final volume
