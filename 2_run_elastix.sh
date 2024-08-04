f0a1cfbccbfadaf963f20a6f731760dc2a708bdb
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-08-04 15:48:34 +0200

    Add elastix run parameters to this repo, tell elastix to use them

--- 2_run_elastix.sh
+++ 2_run_elastix.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 for fn in $@; do
-    run_elastix "$fn" -t template.nrrd -s 64 -w 400 -n
+    run_elastix \
+        "$fn" \
+        -t target.nrrd \
+        -ap "$SCRIPT_DIR/elastixParams/elastixParams_affine.txt" \
+        -bp "$SCRIPT_DIR/elastixParams/elastixParams_Bspline.txt" \
+        -s 64 \
+        -w 400 \
+        -n
 done
