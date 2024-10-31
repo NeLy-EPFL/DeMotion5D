2bb473e6fb0b23887e6bd567fdc4666e14d7a42b
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-10-31 17:07:59 +0100

    Use a mask on the target if `target_mask.nrrd` if present

--- 2_run_elastix.sh
+++ 2_run_elastix.sh
@@ -4,11 +4,15 @@
 # 2_run_elastix.sh /path/to/scan1.nrrd /path/to/scan2.nrrd ...
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -f "target_mask.nrrd" ]; then
+    mask="-tm target_mask.nrrd"
+fi
 
 for fn in $@; do
     run_elastix \
         "$fn" \
         -t target.nrrd \
+        $mask \
         -ap "$SCRIPT_DIR/elastixParams/elastixParams_affine.txt" \
         -bp "$SCRIPT_DIR/elastixParams/elastixParams_Bspline.txt" \
         -s 48 \
